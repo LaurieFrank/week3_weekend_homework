f98d90692dbb8ecb5e49b8186e8ac811a951abe9
Laurie Frank <laurie360@hotmail.co.uk>
2019-09-27 19:13:10 +0100

    update method added to models

--- models/films.rb
+++ models/films.rb
@@ -20,4 +20,10 @@ class Film
     @id = film['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE customers SET (name, funds) = ($1, $2) WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+  
 end
--- models/tickets.rb
+++ models/tickets.rb
@@ -19,4 +19,9 @@ class Ticket
     @id = film['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE tickets SET (customer_id, film_id) = ($1, $2) WHERE id = $3"
+    values = [@customer_id, @film_id]
+    SqlRunner.run(sql, values)
+  end
 end
